3f19b5fe33e450e3963e6c68fa802c0c96c2e12a
cassie_c <cassie.nightingale.13.13@gmail.com>
2024-05-15 14:15:01 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// fetch(https://raw.githubusercontent.com/devchallenges-io/web-project-ideas/main/front-end-projects/data/simple-coffee-listing-data.json)
+//   .then(response => response.json())
+//   .then(data => {
+//     console.log(data)
+//   })
+//   .catch(error => {
+//     console.log(error)
+//   })
